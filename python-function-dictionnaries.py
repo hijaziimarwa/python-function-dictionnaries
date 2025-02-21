d1eac51d0c9aa73432d4a15f688d4933d29dfa12
hijaziimarwa <hijaziimarwa@gmail.com>
2025-02-21 23:37:12 +0200

    Create dictionnary to store percentage

--- python-function-dictionnaries.py
+++ python-function-dictionnaries.py
@@ -7,3 +7,9 @@ def calculate_expense(salary, percentage):
 saving_percentage = float(input(" Enter the saving percentage : "))
 rent_percentage = float(input(" Enter the rent percentage : "))
 electricity_percentage = float(input(" Enter the electricity percentage : "))
+
+expenses = {
+    'saving': calculate_expense(salary, saving_percentage),
+    'rent': calculate_expense(salary, rent_percentage),
+    'electricity': calculate_expense(salary, electricity_percentage)
+}
