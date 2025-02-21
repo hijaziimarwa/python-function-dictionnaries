8969983ed07826e7c8bd72ea55ab6e8b4598ad2b
hijaziimarwa <hijaziimarwa@gmail.com>
2025-02-21 23:43:50 +0200

    Output all results in readable format

--- python-function-dictionnaries.py
+++ python-function-dictionnaries.py
@@ -23,3 +23,14 @@ yearly_electricity = expenses['electricity'] * 12
 additional_saving = 50
 left_saving = additional_saving / expenses['saving']
 
+salary_power_2 = salary ** 2
+
+print(f" The saving is {expenses['saving']}")
+print(f" The rent is {expenses['rent']}")
+print(f" The electricity is {expenses['electricity']}")
+print(f" The total expenses are {total_expenses}")
+print(f" The salary remaining is {salary_remaining}")
+print(f" The yearly rent is {yearly_rent}")
+print(f" The yearly electricity is {yearly_electricity}")
+print(f" The salary power 2 is {salary_power_2}")
+print(f" The left saving is {left_saving}")
