d6ba4f6724c5167a5bf550680cc2eb213ac20791
hijaziimarwa <hijaziimarwa@gmail.com>
2025-02-21 23:34:27 +0200

    Input percentages for saving, rent, and electricity

--- python-function-dictionnaries.py
+++ python-function-dictionnaries.py
@@ -3,3 +3,7 @@ month_name = input(" Enter the month name :")
 
 def calculate_expense(salary, percentage):
     return (salary * percentage) / 100
+
+saving_percentage = float(input(" Enter the saving percentage : "))
+rent_percentage = float(input(" Enter the rent percentage : "))
+electricity_percentage = float(input(" Enter the electricity percentage : "))
